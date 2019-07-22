5af14cf0637bcfd4239bb151102f0c6467aad780
EricK <erickarugu32@gmail.com>
2019-07-22 15:01:33 +0300

    updated

--- newapp.py
+++ newapp.py
@@ -4,11 +4,6 @@ import os
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'MyDB'
-
 @app.route('/home')
 def home():
 	if not session.get('logged_in'):
