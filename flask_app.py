f4828a3e433d0dcc11f487282e44eaa5390c1999
EricK <erickarugu32@gmail.com>
2019-07-31 13:25:36 +0300

    updated the web app

---
+++ flask_app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'bebc92367e831fa89a5bc4aabb1e1785'
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('home.html', title='Home')
+
+@app.route('/about')
+def about():
+	return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+			flash(f'{form.email.data} You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash(f'Login Unsuccessful. Please check your Email and Password', 'danger')
+	return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+	app.run(debug=True)
