09ecdaaa13a8db1ffcb756aaf31c68f9a3ddf951
EricK <erickarugu32@gmail.com>
2019-08-02 17:16:51 +0300

    added bootstrap modal functionality

--- flask_app.py
+++ flask_app.py
@@ -4,33 +4,27 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bebc92367e831fa89a5bc4aabb1e1785'
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-	return render_template('home.html', title='Home')
-
-@app.route('/about')
-def about():
-	return render_template('about.html', title='About')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
 		flash(f'Account created for {form.username.data}!', 'success')
 		return redirect(url_for('home'))
-	return render_template('register.html', title='Register', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-			flash(f'{form.email.data} You have been logged in!', 'success')
+	login_form = LoginForm()
+	if login_form.validate_on_submit():
+		if login_form.email.data == 'admin@blog.com' and login_form.password.data == 'password':
+			flash(f'Welcome {login_form.email.data}!', 'success')
 			return redirect(url_for('home'))
 		else:
 			flash(f'Login Unsuccessful. Please check your Email and Password', 'danger')
-	return render_template('login.html', title='Login', form=form)
+	return render_template('home.html', title='Home', form=form, login_form=login_form)
+
+
+@app.route('/about')
+def about():
+	return render_template('about_us.html', title='About')
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-	email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	username = StringField('Username *', validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email *', validators=[DataRequired(), Email()])
+	user_type = SelectField('User Type *', choices=[('business_owner','Business Owner'), ('customer','Customer')], validators=[DataRequired()])
+	password = PasswordField('Password *', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password *', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
